134b3d38b19dded1d7faeb441388470f5b7c838d
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2023-01-02 20:10:15 +0200

    JavaScript ready

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
+const result = document.querySelector('.result');
+const buttons = document.querySelectorAll('.grid button');
 
+buttons.forEach(b => b.addEventListener('click', clickHandler))
 
+function clickHandler(e) {
+
+    if (e.target.innerText == '=') {
+        try {
+            result.innerText = eval(result.innerText)
+        } catch (error) {
+            result.innerText = 'Invalid Input '
+        }
+    } else if (e.target.innerText == 'C') {
+        result.innerText = ''
+    } else if (e.target.innerText == '←') {
+        result.innerText = result.innerText.slice(0, -1)
+    } else {
+        result.innerText += e.target.innerText;
+    }
+}
